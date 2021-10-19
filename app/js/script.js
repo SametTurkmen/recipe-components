6e445c979fbb60491705a48fa161d1043be06bc9
SametTurkmen <samettrkmn.pc42@gmail.com>
2021-10-20 02:24:46 +0300

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".component--recipe-6 .mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".component--recipe-6 .next-btn",
+        prevEl: ".component--recipe-6 .prev-btn",
+      },
+  });
